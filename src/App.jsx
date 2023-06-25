6876056d41d15ebd79d292dfd462848378d8d756
woojung007 <eeeeve007@gmail.com>
2023-06-25 16:43:37 +0900

    feat: react-router의 outlet 사용하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import RouterPage from 'React-2305/project/router/RouterPage';
-import './App.css';
+import NotFound from 'React-2305/project/router/pages/NotFound';
+import Root from 'React-2305/project/router/pages/Root';
+import Videos from 'React-2305/project/router/pages/Videos';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import './App.css';
+import Home from 'React-2305/project/router/pages/Home';
 
 const App = () => {
     const router = createBrowserRouter([
         {
             path: '/',
-            element: <RouterPage />,
-            errorElement: <p>Not Found😅</p>,
-        },
-        {
-            path: '/videos',
-            element: <p>dfsdf</p>,
+            element: <Root />,
+            errorElement: <NotFound />,
+            children: [
+                { index: true, element: <Home /> },
+                { path: '/videos', element: <Videos /> },
+            ],
         },
     ]);
     return (
