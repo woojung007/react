64bc003e1cc47f8fbbe646551994254a6fa53fdb
woojung007 <eeeeve007@gmail.com>
2023-07-18 23:18:19 +0900

    feat: todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import './App.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import MainProducts from 'React-23/libraries/query/components/MainProducts';
+import TodoPage from 'React-23/project/todo/TodoPage';
 
 const queryClient = new QueryClient();
 
 const App = () => {
     return (
         <QueryClientProvider client={queryClient}>
-            <MainProducts />
+            <TodoPage />
             <ReactQueryDevtools initialIsOpen={true} />
         </QueryClientProvider>
     );
